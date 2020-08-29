ad915419165453eceaae28f68581738cf1402ba7
Nikhil <37348724+Nikhil-K-Singh@users.noreply.github.com>
2020-08-29 18:46:26 +0530

    will now save in PDF format as well

--- main.py
+++ main.py
@@ -1,78 +1,12 @@
-import cv2
 import numpy as np
+import cv2
 
-
-widthImg,heightImg = 480,640
-
-frameWidth = 640
-frameHeight = 480
-
-
-cap = cv2.VideoCapture(0)   # change from 0 to 1 / -1 if an external webcam is used
-cap.set(3,frameWidth) # width with id=3
-cap.set(4,frameHeight) # height with id=4
-cap.set(10,150) # brightness with id=10
-
-
-def preprocessing(img):
-    """
-    - convert to grayscale
-    - apply blur 
-    - detect edges
-
-    """
-    imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    imgBlur=cv2.GaussianBlur(imgGray,(5,5),1)
-    imgCanny=cv2.Canny(imgBlur,200,200)
-
-    # use dilation and erosion combination
-    # to make the edges appropriate width
-    # and overcome shadow conditions
-
-    kernel = np.ones((5,5))
-    imgDilated=cv2.dilate(imgCanny,kernel,iterations=2)
-    imgThreshold=cv2.erode(imgDilated,kernel,iterations=1)
-
-    return imgThreshold
-
-
-def getContours(img):
-    biggest= np.array([])
-    maxArea=0
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area>5000:           ##ref~1
-            # cv2.drawContours(imgContour,cnt,-1,(255,0,0),3)
-            peri=cv2.arcLength(cnt,True)
-            approx=cv2.cv2.approxPolyDP(cnt,0.02*peri,True) # approximation of corner point
-            if area>maxArea and len(approx)==4: # make sure that the scanned object this a quadrilateral
-                                                # and has the max area (thereby the focus of teh user)
-                biggest=approx
-                maxArea=area
-    cv2.drawContours(imgContour,biggest,-1,(255,0,0),20)
-    return biggest
-
-
-def reorder(myPoints):
-    myPoints = myPoints.reshape((4,2))
-    myPointsNew=np.zeros((4,1,2),np.int32)
-
-    add = myPoints.sum(axis=1)
-
-    myPointsNew[0]=myPoints[np.argmin(add)]
-    myPointsNew[3]=myPoints[np.argmax(add)]
-
-    dif = np.diff(myPoints,axis=1)
-    
-    myPointsNew[1]=myPoints[np.argmin(dif)]
-    myPointsNew[2]=myPoints[np.argmin(dif)]
-
-    return myPointsNew
+CAP = cv2.VideoCapture(0)   # change from 0 to 1 / -1 if an external webcam is used
 
 
 def getWarp(img,biggest):
     biggest=reorder(biggest)
+    imgWarped=getWarp(img,biggest)
 
     pts1 = np.float32(biggest)
 
@@ -98,9 +32,80 @@ def getWarp(img,biggest):
     return imgCropped
 
 
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4,2))
+    myPointsNew=np.zeros((4,1,2),np.int32)
+
+    add = myPoints.sum(axis=1)
+
+    myPointsNew[0]=myPoints[np.argmin(add)]
+    myPointsNew[3]=myPoints[np.argmax(add)]
+
+    dif = np.diff(myPoints,axis=1)
+    
+    myPointsNew[1]=myPoints[np.argmin(dif)]
+    myPointsNew[2]=myPoints[np.argmin(dif)]
+
+    return myPointsNew
+
+
+def getContours(img):
+    biggest= np.array([])
+    maxArea=0
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>5000:           ##ref~1
+            # cv2.drawContours(imgContour,cnt,-1,(255,0,0),3)
+            peri=cv2.arcLength(cnt,True)
+            approx=cv2.cv2.approxPolyDP(cnt,0.02*peri,True) # approximation of corner point
+            if area>maxArea and len(approx)==4: # make sure that the scanned object this a quadrilateral
+                                                # and has the max area (thereby the focus of teh user)
+                biggest=approx
+                maxArea=area
+    cv2.drawContours(imgContour,biggest,-1,(255,0,0),20)
+    return biggest
+
+
+
+def preprocessing(img):
+    """
+    - convert to grayscale
+    - apply blur 
+    - detect edges
+
+    """
+    imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur=cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny=cv2.Canny(imgBlur,200,200)
+
+    # use dilation and erosion combination
+    # to make the edges appropriate width
+    # and overcome shadow conditions
+
+    kernel = np.ones((5,5))
+    imgDilated=cv2.dilate(imgCanny,kernel,iterations=2)
+    imgThreshold=cv2.erode(imgDilated,kernel,iterations=1)
+
+    return imgThreshold
+
+# Setting the basic CONSTANTS
+
+widthImg,heightImg = 480,640
+
+frameWidth = 640
+frameHeight = 480
+
+
+#Proceeding with the frame capturing
+
+CAP.set(3,frameWidth) # width with id=3
+CAP.set(4,frameHeight) # height with id=4
+CAP.set(10,150) # brightness with id=10
+
 
 while True:
-    success, img = cap.read()
+    success, img = CAP.read()
     img=cv2.resize(img,(widthImg,heightImg))
 
     imgContour=img.copy()
@@ -119,16 +124,25 @@ while True:
 
     if biggest.size !=0:
         imgWarped=getWarp(img,biggest)
-        cv2.imshow("Document Detected", imgWarped)
+        cv2.imshow("Document Detected", imgWarped)    # this will be true only  if contour is large enough ## ref~1
         save_file_name = input('''
-        File name to save with .jpg extension
+        File name to save with .jpg extension 
 
         share the path if dir is to be changed
         
         ''')
         cv2.imwrite(save_file_name,imgWarped)
+        pdf_check=input("do you want a pdf save as well [y/n] :\t")
+        if pdf_check.lower() in ["y" or "yes"]:
+            from PIL import Image
+            im=Image.open(save_file_name+".jpg")
+            im =im.convert('RGB')
+            im.save(f'{save_file_name}.pdf')            
     else:
         cv2.imshow("Image",img)
+        cv2.imshow("Thresholded",imgThreshold)
+        # cv2.imshow("Warped",imgWarped) # the warping will be done only if documnet is detected
+
   
     if cv2.waitKey(1) and 0xFF == ord('q'):
         break
